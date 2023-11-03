7ed2fbb85bbb214d1c415743ca7df95525b9f485
khushibhadani10 <141472246+khushibhadani10@users.noreply.github.com>
2023-11-03 09:33:35 +0000

    data type

---
+++ 1_basic/basic_code.js
@@ -0,0 +1,18 @@
+const accountId = 12345
+let accountEmail = "xyz@gmail.com"
+var accountPassword = "gbcdfsdefc"
+accountCity = "Surat"
+let accountContry;
+
+// accountId = 23455
+accountEmail = "ishika@ishkagmail.com"
+accountPassword = "222222"
+accountCity = "seoul"
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.log(accountContry);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountContry])
---
+++ 1_basic/datatype.js
@@ -0,0 +1,10 @@
+"use strict";
+// alert("hello");
+let age =20
+console.log(3+5+6)
+
+console.log(3/3+6-9*12)
+
+console.log("KHUSHI & ISHIKA")
+
+console.log(typeof age)
