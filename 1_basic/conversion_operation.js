753876237da20a63f436c41563b9a1a7307c2bd4
khushibhadani10 <141472246+khushibhadani10@users.noreply.github.com>
2023-12-01 05:34:33 +0000

    conversion

---
+++ 1_basic/conversion_operation.js
@@ -0,0 +1,29 @@
+let score = 'ff34';
+ 
+// console.log(typeof(score));
+// console.log(score);
+
+let valueOfNum = Number(score);
+
+// console.log(typeof(valueOfNum));
+// console.log(valueOfNum);
+let isLoggedin = '0';
+// console.log(isLoggedin);
+let boolValue = Boolean(isLoggedin);
+
+// console.log(boolValue);
+
+//true => 1;
+//false => 0;
+//true => 'hitesh';
+//false => '';
+
+let number = 33;
+
+console.log(typeof number);
+console.log(number);
+
+let stringToNum = String(number);
+
+console.log(stringToNum);
+console.log(typeof stringToNum);
